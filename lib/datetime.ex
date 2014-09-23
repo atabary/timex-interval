b94d4c18ff9a4c451b257d86a4ec4b164ee0838d
Alexis Tabary <alexis.tabary@gmail.com>
2014-09-23 18:11:34 +0900

    DateTimeInterval now implements the Enumerable protocol

--- lib/datetime.ex
+++ lib/datetime.ex
@@ -8,8 +8,8 @@ defmodule TimexInterval.DateTimeInterval do
   defstruct start_instant: @default_start,
             end_instant:   @default_end,
             start_open:    false,
-            end_open:      true
-
+            end_open:      true,
+            step:          [days: 1]
 
   @doc """
   Make a new interval given a DateTime objects and a shift, or two DateTime objects
@@ -25,7 +25,17 @@ defmodule TimexInterval.DateTimeInterval do
   end
 
   @doc """
-  Iterate over a DateTimeInterval structure, by default over each day.
+  Update the step for this interval.
+
+  The step should be a keyword list: anything that is valid for the Timex.Date.shift function,
+  which is used underneath.
+  """
+  def set_step(interval, step) do
+    %TimexInterval.DateTimeInterval{interval | step: step}
+  end
+
+  @doc """
+  Return a list of DateTime that spans the DateTimeInterval structure, by default with steps of one day.
 
   The keyword list can be anything that is valid for he Timex.Date.shift function, which is used underneath.
 
@@ -33,18 +43,18 @@ defmodule TimexInterval.DateTimeInterval do
 
     > use Timex
     > DateTimeInterval.new(Date.from({2014, 9, 22}), Date.from({2014, 9, 29}))
-      |> DateTimeInterval.iterate()
+      |> DateTimeInterval.to_list()
       |> Enum.map(fn(dt) -> DateFormat.format!(dt, "%Y-%m-%d", :strftime) end)
     #> ["2014-09-22", "2014-09-23", "2014-09-24", "2014-09-25", "2014-09-26", "2014-09-27", "2014-09-28"]
 
   """
-  def iterate(interval, keywords \\ [days: 1]) do
+  def to_list(interval) do
     if interval.start_open do
-      Date.shift(interval.start_instant, keywords)
+      Date.shift(interval.start_instant, interval.step)
     else
       interval.start_instant
     end
-    |> iterate(interval.end_instant, interval.end_open, keywords, [])
+    |> to_list(interval.end_instant, interval.end_open, interval.step, [])
   end
 
   @doc """
@@ -67,17 +77,43 @@ defmodule TimexInterval.DateTimeInterval do
     s1 <> s2 <> s3 <> s4 <> s5
   end
 
+
   ## Private
 
-  defp iterate(current_date, end_date, end_open, keywords, enumeration) do
+  defp to_list(current_date, end_date, end_open, keywords, enumeration) do
     if has_recursion_ended?(current_date, end_date, end_open) do
       Enum.reverse(enumeration)
     else
       next_date = Date.shift(current_date, keywords)
-      iterate(next_date, end_date, end_open, keywords, [current_date|enumeration])
+      to_list(next_date, end_date, end_open, keywords, [current_date|enumeration])
+    end
+  end
+
+  defp to_list(current_date, end_date, end_open, keywords, enumeration) do
+    if has_recursion_ended?(current_date, end_date, end_open) do
+      Enum.reverse(enumeration)
+    else
+      next_date = Date.shift(current_date, keywords)
+      to_list(next_date, end_date, end_open, keywords, [current_date|enumeration])
     end
   end
 
   defp has_recursion_ended?(current_date, end_date,  true), do: Date.compare(end_date, current_date) <= 0
   defp has_recursion_ended?(current_date, end_date, false), do: Date.compare(end_date, current_date) <  0
+end
+
+defimpl Enumerable, for: TimexInterval.DateTimeInterval do
+  def reduce(interval, acc, fun) do
+    do_reduce(TimexInterval.DateTimeInterval.to_list(interval), acc, fun)
+  end
+
+  defp do_reduce(_,     {:halt, acc}, _fun),   do: {:halted, acc}
+  defp do_reduce(list,  {:suspend, acc}, fun), do: {:suspended, acc, &do_reduce(list, &1, fun)}
+  defp do_reduce([],    {:cont, acc}, _fun),   do: {:done, acc}
+  defp do_reduce([h|t], {:cont, acc}, fun),    do: do_reduce(t, fun.(h, acc), fun)
+
+  def member?(_list, _value),
+    do: {:error, __MODULE__}
+  def count(_list),
+    do: {:error, __MODULE__}
 end
