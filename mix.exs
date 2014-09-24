6cab8a856eca1a48c75961a9957dade5db2cff20
Alexis Tabary <alexis.tabary@gmail.com>
2014-09-24 14:20:39 +0900

    Fix links in mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TimexInterval.Mixfile do
 
   def project do
     [ app: :timex_interval,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: ">=1.0.0",
       description: "A date/time interval library for Elixir projects, based on Timex.",
       package: package,
@@ -19,8 +19,6 @@ defmodule TimexInterval.Mixfile do
     [ files: ["lib", "mix.exs", "README.md", "LICENSE"],
       contributors: ["Alexis Tabary"],
       licenses: ["Apache 2"],
-      links: [
-        {"GitHub", "https://github.com/atabary/timex-interval"},
-      ] ]
+      links: %{"GitHub" => "https://github.com/atabary/timex-interval"} ]
   end
 end
